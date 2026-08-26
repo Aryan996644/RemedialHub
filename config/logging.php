f69ef463ab749545291e4effaa91b723c103e31a
mdkamarrzakhan <152802475+Aryan996644@users.noreply.github.com>
2026-08-26 23:25:47 +0530

    Update logging config to fallback to stderr and /tmp/storage

--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) ? 'stderr' : 'stack'),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,20 +54,20 @@ return [
 
         'stack' => [
             'driver' => 'stack',
-            'channels' => explode(',', (string) env('LOG_STACK', 'single')),
+            'channels' => explode(',', (string) env('LOG_STACK', (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) ? 'stderr' : 'single')),
             'ignore_exceptions' => false,
         ],
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => env('LOG_PATH', (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) ? '/tmp/storage/logs/laravel.log' : storage_path('logs/laravel.log')),
             'level' => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
         ],
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => env('LOG_PATH', (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) ? '/tmp/storage/logs/laravel.log' : storage_path('logs/laravel.log')),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => env('LOG_DAILY_DAYS', 14),
             'replace_placeholders' => true,
